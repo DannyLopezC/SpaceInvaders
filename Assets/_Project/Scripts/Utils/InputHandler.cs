243775747e8fd1a95593a7f5d81a4ecfae833718
Danny López Cárdenas <danny.lopezc00@gmail.com>
2025-08-07 09:17:57 -0500

    Input system for space invaders

--- Assets/_Project/Scripts/Utils/InputHandler.cs
+++ Assets/_Project/Scripts/Utils/InputHandler.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public interface IInputHandler {
-    public event Action OnPlayerClickInteraction;
+    event Action<Vector2> OnMoveInput;
+    event Action OnShootInput;
 }
 
 public class InputHandler : IInputHandler {
-    public event Action OnPlayerClickInteraction;
+    public event Action<Vector2> OnMoveInput;
+    public event Action OnShootInput;
 
     private readonly InputSystem_Actions _inputSystemActions;
 
@@ -18,10 +20,17 @@ public class InputHandler : IInputHandler {
     }
 
     private void AddListeners() {
-        _inputSystemActions.Player.Attack.performed += OnPlayerClick;
+        _inputSystemActions.Player.Move.performed += OnMove;
+        _inputSystemActions.Player.Move.canceled += OnMove;
+        _inputSystemActions.Player.Attack.performed += OnShoot;
     }
 
-    private void OnPlayerClick(InputAction.CallbackContext callbackContext) {
-        OnPlayerClickInteraction?.Invoke();
+    private void OnMove(InputAction.CallbackContext context) {
+        Vector2 moveValue = context.ReadValue<Vector2>();
+        OnMoveInput?.Invoke(moveValue);
+    }
+
+    private void OnShoot(InputAction.CallbackContext context) {
+        OnShootInput?.Invoke();
     }
 }
