63aed7c0ec655c989100783b910517c42b7923c5
Danny López Cárdenas <danny.lopezc00@gmail.com>
2025-08-07 09:23:14 -0500

    View and Controller base classes

---
+++ Assets/_Project/Scripts/Utils/MonoBehaviourController.cs
@@ -0,0 +1,30 @@
+public interface IMonoBehaviourController {
+    void OnAwake();
+    void OnStart();
+    void OnUpdate();
+    void OnFixedUpdate();
+    void OnDestroy();
+}
+
+public class MonoBehaviourController : IMonoBehaviourController {
+    protected readonly IMonoBehaviourView View;
+
+    public MonoBehaviourController(IMonoBehaviourView view) {
+        View = view;
+    }
+
+    public virtual void OnAwake() {
+    }
+
+    public virtual void OnStart() {
+    }
+
+    public virtual void OnUpdate() {
+    }
+
+    public virtual void OnFixedUpdate() {
+    }
+
+    public virtual void OnDestroy() {
+    }
+}
---
+++ Assets/_Project/Scripts/Utils/MonoBehaviourView.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public interface IMonoBehaviourView {
+    public Transform Transform { get; }
+    public GameObject GameObject { get; }
+    public MonoBehaviour MonoBehaviour { get; }
+}
+
+public abstract class MonoBehaviourView : MonoBehaviour, IMonoBehaviourView {
+    public Transform Transform => transform;
+    public GameObject GameObject => gameObject;
+    public MonoBehaviour MonoBehaviour => this;
+
+    protected abstract IMonoBehaviourController Controller();
+
+    protected virtual void Awake() {
+        CreateController();
+        Controller().OnAwake();
+    }
+
+    protected virtual void Start() {
+        Controller().OnStart();
+    }
+
+    protected virtual void Update() {
+        Controller().OnUpdate();
+    }
+
+    protected virtual void FixedUpdate() {
+        Controller().OnFixedUpdate();
+    }
+
+    protected virtual void OnDestroy() {
+        Controller().OnDestroy();
+    }
+
+    protected abstract void CreateController();
+}
